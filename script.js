3e69a905d4a6955f9bcc77e332e651fc2d4f28a3
Adriano Teodoro <adrianopteodoro@gmail.com>
2024-12-20 17:52:29 -0300

    Add locale support

--- script.js
+++ script.js
@@ -5,6 +5,10 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const dateFormat = urlParams.get("dateFormat") || 'ddd DD MMM yyyy hh:mm:ss A';
+const dateLocale = urlParams.get("dateLocale") || 'en';
+
+// GLOBALLY SET LOCALE
+moment.locale(dateLocale);
 
 ///////////////
 // FUNCTIONS //
